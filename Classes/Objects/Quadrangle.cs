48ea44e33e7f1192d7593351fea7a153a9f7b10d
Ірина Левдик <Levdyk.I.A@nmu.one>
2020-03-09 18:51:20 +0200

    Добавьте файлы проекта.

---
+++ Classes/Objects/Quadrangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnvelopesTask.Classes.Objects
+{
+    abstract class Quadrangle
+    {
+        #region
+
+        private const int MAX_QUANTITY = 4;
+
+        #endregion
+
+        public double[] sides;
+
+        public double[] angles;
+        
+        public Quadrangle()
+        {
+            sides = new double[MAX_QUANTITY];
+            angles = new double[MAX_QUANTITY];
+            sides.Initialize();
+        }
+    }
+}
